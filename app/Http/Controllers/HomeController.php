128879d210823b407872df1c9ea6a2694093723a
intern-tieumomo <intern.tieumomo@gmail.com>
2021-04-16 03:59:38 +0700

    Fix quotes errors in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\SubCategory;
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Kreait\Firebase\Factory;
 
 class HomeController extends Controller
@@ -14,8 +14,18 @@ class HomeController extends Controller
     public function index()
     {
         $menuCategories = Category::limit(7)->get();
-        $quotes = Inspiring::quotes();
-
+        $quotes = Collection::make([
+            'An unexamined life is not worth living. - Socrates',
+            'Be present above all else. - Naval Ravikant',
+            'He who is contented is rich. - Laozi',
+            'No surplus words or unnecessary actions. - Marcus Aurelius',
+            'Order your soul. Reduce your wants. - Augustine',
+            'Simplicity is an acquired taste. - Katharine Gerould',
+            'Simplicity is the essence of happiness. - Cedric Bledsoe',
+            'Simplicity is the ultimate sophistication. - Leonardo da Vinci',
+            'Smile, breathe, and go slowly. - Thich Nhat Hanh',
+            'Well begun is half done. - Aristotle',
+        ]);
 
         return view('home', [
             'menuCategories' => $menuCategories,
