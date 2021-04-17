72eef22451497bc878382974d637c35756f7ca05
code-tieumomo <code.tieumomo@gmail.com>
2021-04-17 19:21:18 +0700

    Post detail page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,32 +1,153 @@
 @extends('app')
 
 @section('title')
-	{{ $post->title }} | UET-News
+    {{ $post->title }} | UET-News
 @endsection
 
 @section('headline')
-	<div class="container">
-		<div class="headline bg0 flex-wr-sb-c p-rl-20 p-tb-8">
-			<div class="f2-s-1 p-r-30 m-tb-6">
-				<a href="index.html" class="breadcrumb-item f1-s-3 cl9">
-					Home
-				</a>
+    <div class="container">
+        <div class="headline bg0 flex-wr-sb-c p-rl-20 p-tb-8">
+            <div class="f2-s-1 p-r-30 m-tb-6">
+                <a href="{{ route('home.index') }}" class="breadcrumb-item f1-s-3 cl9">
+                    Home
+                </a>
 
-				<a href="blog-list-01.html" class="breadcrumb-item f1-s-3 cl9">
-					Blog 
-				</a>
+                <a href="{{ $post->subCategory->category->id }}" class="breadcrumb-item f1-s-3 cl9">
+                    {{ $post->subCategory->category->name }}
+                </a>
 
-				<span class="breadcrumb-item f1-s-3 cl9">
-					 Nulla non interdum metus non laoreet nisi tellus eget aliquam lorem pellentesque
-				</span>
-			</div>
+                <a href="{{ $post->subCategory->id }}" class="breadcrumb-item f1-s-3 cl9">
+                    {{ $post->subCategory->name }}
+                </a>
 
-			<div class="pos-relative size-a-2 bo-1-rad-22 of-hidden bocl11 m-tb-6">
-				<input class="f1-s-1 cl6 plh9 s-full p-l-25 p-r-45" type="text" name="search" placeholder="Search">
-				<button class="flex-c-c size-a-1 ab-t-r fs-20 cl2 hov-cl10 trans-03">
-					<i class="zmdi zmdi-search"></i>
-				</button>
-			</div>
-		</div>
-	</div>
+                <span class="breadcrumb-item f1-s-3 cl9">
+                     {{ $post->title }}
+                </span>
+            </div>
+
+            <div class="pos-relative size-a-2 bo-1-rad-22 of-hidden bocl11 m-tb-6">
+                <input class="f1-s-1 cl6 plh9 s-full p-l-25 p-r-45" type="text" name="search" placeholder="Search">
+                <button class="flex-c-c size-a-1 ab-t-r fs-20 cl2 hov-cl10 trans-03">
+                    <i class="zmdi zmdi-search"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+    <!-- Content -->
+    <section class="bg0 p-b-70 p-t-5">
+        <!-- Title -->
+        <div class="bg-img1 size-a-18 how-overlay1" style="background-image: url('{{ $post->thumbnail }}');">
+            <div class="container h-full flex-col-e-c p-b-58">
+                <a href="#" class="f1-s-10 cl0 hov-cl10 trans-03 text-uppercase txt-center m-b-33">
+                    {{ $post->subCategory->category->name }}
+                </a>
+
+                <h3 class="f1-l-5 cl0 p-b-16 txt-center respon2">
+                    {{ $post->title }}
+                </h3>
+
+                <div class="flex-wr-c-s">
+                    <span class="f1-s-3 cl8 m-rl-7 txt-center">
+                        <a href="#" class="f1-s-4 cl8 hov-cl10 trans-03">
+                            by {{ $post->user->name }}
+                        </a>
+
+                        <span class="m-rl-3">-</span>
+
+                        <span>
+                            {{ $post->created_at->toFormattedDateString() }}
+                        </span>
+                    </span>
+
+                    <span class="f1-s-3 cl8 m-rl-7 txt-center">
+                        5239 Views
+                    </span>
+
+                    <a href="" class="f1-s-3 cl8 m-rl-7 txt-center hov-cl10 trans-03">
+                        {{ $post->comments->count() }} Comment
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Detail -->
+        <div class="container p-t-82">
+            <div class="row justify-content-center">
+                <div class="col-md-10 col-lg-8 p-b-100">
+                    <div class="p-r-10 p-r-0-sr991">
+                        <!-- Blog Detail -->
+                        <div class="p-b-70">
+                            {{ $post->content }}
+
+                            <!-- Info -->
+                            <div class="flex-s-s p-t-12 p-b-15">
+                                <span class="f1-s-12 cl5 m-r-8">
+                                    Writer:
+                                </span>
+                                
+                                <div class="flex-wr-s-s size-w-0">
+                                    <a href="#" class="f1-s-12 cl8 hov-link1">
+                                        {{ $post->user->name }} |
+                                    </a>
+                                </div>
+
+                                <span class="f1-s-12 cl5 m-r-8">
+                                    Sub Category:
+                                </span>
+                                
+                                <div class="flex-wr-s-s size-w-0">
+                                    <a href="#" class="f1-s-12 cl8 hov-link1 m-r-15">
+                                        {{ $post->subCategory->name }}
+                                    </a>
+                                </div>
+                            </div>
+
+                            <!-- Share -->
+                            <div class="flex-s-s">
+                                <span class="f1-s-12 cl5 p-t-1 m-r-15">
+                                    Share:
+                                </span>
+                                
+                                <div class="flex-wr-s-s size-w-0">
+                                    <a href="#" class="dis-block f1-s-13 cl0 bg-facebook borad-3 p-tb-4 p-rl-18 hov-btn1 m-r-3 m-b-3 trans-03">
+                                        <i class="fab fa-facebook-f m-r-7"></i>
+                                        Facebook
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Leave a comment -->
+                        <div>
+                            <h4 class="f1-l-4 cl3 p-b-12">
+                                Comment
+                            </h4>
+
+                            <form>
+                                <textarea class="bo-1-rad-3 bocl13 size-a-21 f1-s-13 cl5 plh6 p-rl-18 p-tb-14 m-b-20" name="msg" placeholder="Comment..."></textarea>
+
+                                <button class="size-a-17 bg2 borad-3 f1-s-12 cl0 hov-btn1 trans-03 p-rl-15 m-t-10">
+                                    Post Comment
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-md-10 col-lg-4 p-b-100">
+                    <div class="p-l-10 p-rl-0-sr991">
+                        <!-- Banner -->
+                        <div class="flex-c-s">
+                            <a href="#">
+                                <img class="max-w-full" src="{{ asset('client-assets/images/banner-02.jpg') }}" alt="IMG">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
